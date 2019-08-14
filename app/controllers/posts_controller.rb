dd1cd5b617e7aed1ec40f4648cbb2305ed0103f4
nkmess <nkmess12@gmail.com>
2019-08-14 13:16:07 +0900

    Add redirect

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
 
+  before_action :move_to_index, except: :index
+
   def index
   end
   
@@ -9,4 +11,8 @@ class PostsController < ApplicationController
   def create
   end
 
+  private
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
+  end
 end
