caeb38eda982ef4c4d63a9727ab003c122a6ff0c
nkmess <nkmess12@gmail.com>
2019-08-20 21:56:00 +0900

    Update view for products#new

--- app/assets/javascripts/post.js
+++ app/assets/javascripts/post.js
@@ -1,3 +1,21 @@
 window.onload = function(){
+
+  
+
+  $(document).on("change", "#image__upload", function(e){
+    var file = e.target.files[0];
+    reader = new FileReader();
+
+    $(".post__form__pic__box#img_field").css('display', 'none');
+    $('.post__form__pic__preview').css('display', 'block');
+    reader.onload = function(e) {
+      $("#preview").attr('src', e.target.result);
+    }
+    reader.readAsDataURL(file);
+  })
+
+
   $("#country_select").select2();
+
+
 };
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,13 +6,21 @@ class PostsController < ApplicationController
   end
   
   def new
+    @post = Post.new
     @countries = ISO3166::Country.all_translated('JA')
   end
 
   def create
+    @post= Post.create(post_params)
   end
 
   private
+  def post_params
+    params.require(:post).permit(:text, :country, :duration, :image).merge(user_id: current_user.id)
+  end
+
+
+
   def move_to_index
     redirect_to action: :index unless user_signed_in?
   end
