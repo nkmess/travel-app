77ddc50ff2fb7ebc53a92003b2cc695e0bf69ef3
nkmess <nkmess12@gmail.com>
2019-08-23 09:55:04 +0900

    Add pagination to top page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   before_action :move_to_index, except: :index
 
   def index
-    @posts = Post.all
+    @posts = Post.order("created_at DESC").page(params[:page]).per(6)
 
     @sidebarAreas = 
       [{area:"Europe", kana:"ヨーロッパ", link:""},
