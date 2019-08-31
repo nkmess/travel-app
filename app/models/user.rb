4e7f63bc929b7285a7542e5efe6a173f9e385de5
nkmess <nkmess12@gmail.com>
2019-08-31 14:39:43 +0900

    test code for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :posts, dependent: :destroy
+
+  validates :name, presence: true, length: { maximum: 6}
   
 end
