24769a17663574a4a73fe011287f7d435ad1f221
nkmess <nkmess12@gmail.com>
2019-09-12 20:41:01 +0900

    fix: Change content of variable

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 
   def show
     @name = current_user.name
-    @posts = Post.order("created_at DESC").page(params[:page]).per(6)
+    @posts = Post.where(user_id: current_user.id).page(params[:page]).per(6)
   end
+
 end
