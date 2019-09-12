657240641152a12c71d16aa274a0f72ad2cb84b7
nkmess <nkmess12@gmail.com>
2019-09-12 09:57:11 +0900

    refactor: Add change routing for mypage liked posts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,5 @@ class UsersController < ApplicationController
   def show
     @name = current_user.name
     @posts = Post.order("created_at DESC").page(params[:page]).per(6)
-    @liked_posts = current_user.liked_posts.page(params[:page]).per(6)
   end
 end
