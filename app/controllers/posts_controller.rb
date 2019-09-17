236c67b25917df9b7f4707b305b4c31d496836d0
nkmess <nkmess12@gmail.com>
2019-09-17 10:09:29 +0900

    refactor: Add change to a filter

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
 
-  before_action :move_to_index, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show, :search]
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
