a25a05e942387978e9abfb951b6fa5d1b5e1e86d
nkmess <nkmess12@gmail.com>
2019-09-23 21:47:23 +0900

    style: Add hover action to area searching

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
 
-  before_action :authenticate_user!, except: [:index, :show, :search]
+  before_action :authenticate_user!, except: [:index, :show, :search, :search_europe, :search_africa, :search_northamerica, :search_latainamerica, :search_asia, :search_oceania, :search_middleeast]
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
