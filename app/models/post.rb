86da0238a632bb0436c210c4167fcc3a2632dd16
nkmess <nkmess12@gmail.com>
2019-09-30 17:57:03 +0900

    refactor: Use scope for searching by area

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,6 +7,15 @@ class Post < ApplicationRecord
 
   scope :recent, -> { order("created_at DESC")}
 
+  scope :europe, -> { where(country: ["208", "250", "528", "578", "643", "724", "752", "826"]) }
+  scope :africa, -> { where(country: ["710, 818"]) }
+  scope :north_america, -> { where(country: ["124", "840"]) }
+  scope :latain_america, -> { where(country: ["032","076", "152"]) }
+  scope :asia, -> { where(country: ["156", "158", "344", "392", "410", "702", "764"]) }
+  scope :oceania, -> { where(country: ["036", "316",  "554"]) }
+  scope :middle_east, -> { where(country: ["682", "792"]) }
+
+
   enum duration: [:in_two_days, :in_three_days, :in_five_days, :in_seven_days,  :in_two_weeks, :in_a_month, :in_three_months, :in_six_months, :in_a_year, :over_a_year]
 
   validates :image, presence: true
