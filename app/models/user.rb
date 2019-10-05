ff77008abaa64b27517dae40ea5837cf0f363ccd
nkmess <nkmess12@gmail.com>
2019-10-05 19:40:24 +0900

    feat: Add omniauthable to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  devise :omniauthable, omniauth_providers: %i[facebook]
 
   has_many :posts, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :liked_posts, through: :likes, source: :post
 
   validates :name, presence: true, length: { maximum: 6}
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end
+  end
 
   
 end
